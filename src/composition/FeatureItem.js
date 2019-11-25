c5a143f026216b6d0e0349a723a2386996a99b44
Erika Reiman <erika.reiman@gmail.com>
2019-11-25 18:19:12 -0500

    Fixed toggling issue with FeatureItem

--- src/composition/FeatureItem.js
+++ src/composition/FeatureItem.js
@@ -1,34 +1,49 @@
-import React from 'react';
-import slugify from 'slugify';
+import React from "react";
+import slugify from "slugify";
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
 });
 
 class FeatureItem extends React.Component {
-  render() {
-    console.log(this.props.selected)
-    const options = this.props.features[this.props.feature].map(item => {
-    const itemHash = slugify(JSON.stringify(item));
-    return (
-      <div key={itemHash} className="feature__item">
-        <input
-          type="radio"
-          id={itemHash}
-          className="feature__option"
-          name={slugify(this.props.feature)}
-          checked={item.name === this.props.selected.name}
-          onChange={e => this.props.handleUpdateFeature(e.target.id)}
-        />
-        <label htmlFor={itemHash} className="feature__label">
-          {item.name} ({USCurrencyFormat.format(item.cost)})
-        </label>
-      </div>
-    );
-  });
-  return (<div>{options}</div>);
+  state = {
+    selected: null
+  };
+
+  componentDidMount() {
+    const feature = this.props.feature;
+    this.setState({
+      selected: feature[0]
+    });
   }
-  
+
+  updateSelected = () => {};
+
+  render() {
+    const { selected } = this.state;
+    console.log(selected);
+    const options = this.props.feature.map((item, index) => {
+      console.log(item);
+      const itemHash = slugify(JSON.stringify(item));
+      console.log(itemHash);
+      return (
+        <div key={itemHash} className="feature__item">
+          <input
+            type="radio"
+            id={itemHash}
+            className="feature__option"
+            name={slugify(JSON.stringify(this.props.feature))}
+            checked={item.name === this.state.selected}
+            onChange={e => this.props.handleUpdateFeature(e.target.id)}
+          />
+          <label htmlFor={itemHash} className="feature__label">
+            {item.name} ({USCurrencyFormat.format(item.cost)})
+          </label>
+        </div>
+      );
+    });
+    return <div>{options}</div>;
+  }
 }
 export default FeatureItem;
--- src/composition/SpecList.js
+++ src/composition/SpecList.js
@@ -1,35 +1,28 @@
-import React from 'react';
-import slugify from 'slugify';
-import FeatureItem from './FeatureItem';
+import React from "react";
+import slugify from "slugify";
+import FeatureItem from "./FeatureItem";
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
 });
 
 class SpecList extends React.Component {
-
-
-
-
   render() {
     const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-    
+      const featureHash = feature + "-" + idx;
+
       return (
         <fieldset className="feature" key={featureHash}>
           <legend className="feature__name">
             <h3>{feature}</h3>
           </legend>
-          <FeatureItem features={this.props.features} selected={this.props.selected} feature={feature} handleUpdateFeature={this.props.handleUpdateFeature} />
+          <FeatureItem feature={this.props.features[feature]} handleUpdateFeature={this.props.handleUpdateFeature} />
         </fieldset>
-    );
-    
-
-    
-      });
-      return (<div>{features}</div>);
+      );
+    });
+    return <div>{features}</div>;
   }
 }
-  
+
 export default SpecList;
