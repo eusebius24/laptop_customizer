3fabce8c9b993f9af9475bfeb6597a70e04817bf
Erika Reiman <erika.reiman@gmail.com>
2019-11-25 09:11:51 -0500

    tried another separation of speclist component

--- src/composition/SpecList.js
+++ src/composition/SpecList.js
@@ -1,18 +1,52 @@
 import React from 'react';
+import slugify from 'slugify';
 
-class SpecList extends React.Component {
-    render() {
-        const features = Object.keys(this.props.features).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const options = this.props.features[feature].map((item) => {})
-        });
-        return(
-            <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {this.props.features}
-          </form>
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
+
+class SpecList extends React.Component{updateFeature = (feature, newValue) => {
+  const selected = Object.assign({}, this.props.selected);
+  selected[feature] = newValue;
+  //   this.setState({
+  //     selected
+  //   });
+  }
+
+
+  render() {
+    const features = Object.keys(this.props.features).map((feature, idx) => {
+      const featureHash = feature + '-' + idx;
+      const options = this.props.features[feature].map(item => {
+        const itemHash = slugify(JSON.stringify(item));
+        return (
+          <div key={itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={itemHash}
+              className="feature__option"
+              name={slugify(feature)}
+              checked={item.name === this.props.selected[feature].name}
+              onChange={e => this.updateFeature(feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+              {item.name} ({USCurrencyFormat.format(item.cost)})
+            </label>
+          </div>
         );
-    }
-}
+      });
 
+      return (
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
+      );
+    });
+  }
+}
+  
 export default SpecList;
