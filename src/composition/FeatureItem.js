c2da14d7f041669361266e42aca851e396779173
Erika Reiman <erika.reiman@gmail.com>
2019-11-25 15:07:47 -0500

    created FeatureItem component

---
+++ src/composition/FeatureItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import slugify from 'slugify';
+
+class FeatureItem extends React.Component {
+  render() {
+    const options = this.props.features[feature].map(item => {
+    const itemHash = slugify(JSON.stringify(item));
+    return (
+      <div key={itemHash} className="feature__item">
+        <input
+          type="radio"
+          id={itemHash}
+          className="feature__option"
+          name={slugify(feature)}
+          checked={item.name === this.props.selected[feature].name}
+          onChange={e => this.props.handleUpdateFeature(e.target.id)}
+        />
+        <label htmlFor={itemHash} className="feature__label">
+          {item.name} ({USCurrencyFormat.format(item.cost)})
+        </label>
+      </div>
+    );
+  });
+  return (<div>{options}</div>);
+  }
+  
+}
+export default FeatureItem;
--- src/composition/SpecList.js
+++ src/composition/SpecList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import slugify from 'slugify';
+import FeatureItem from './FeatureItem';
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -14,36 +15,20 @@ class SpecList extends React.Component {
   render() {
     const features = Object.keys(this.props.features).map((feature, idx) => {
       const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.props.selected[feature].name}
-              onChange={e => this.props.handleUpdateFeature(e.target.id)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
+      
       });
-
+      
       return (
         <fieldset className="feature" key={featureHash}>
           <legend className="feature__name">
             <h3>{feature}</h3>
           </legend>
-          {options}
+          <FeatureItem features={this.props.features} />
         </fieldset>
       );
-    });
+    
 
-    return <div>{features}</div>
+    return (<div>{features}</div>)
   }
 }
   
